5f9a87914084d381086e8c4d9ef7952ff536f53c
Jems2438 <jems2438@gmail.com>
2024-05-24 17:10:07 +0530

    TestCase Added

--- MyShop.Core/Contracts/IBasketService.cs
+++ MyShop.Core/Contracts/IBasketService.cs
@@ -17,5 +17,6 @@ namespace MyShop.Core.Contracts
         List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
 
         BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void ClearBasket(HttpContextBase httpContext);
     }
 }
